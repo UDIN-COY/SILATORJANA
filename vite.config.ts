cc0214084ebd7828d87935428b2ddc6e7048d564
UDIN-K <safrisam.id09@gmail.com>
2026-06-04 03:36:26 +0700

    chore: integrate backend server into dev script and update proxy target to local development environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
@@ -30,13 +30,10 @@ export default defineConfig(({mode}) => {
       },
     },
     server: {
-      port: 3000,
-      host: '0.0.0.0',
-      strictPort: true,
       hmr: process.env.DISABLE_HMR !== 'true',
       proxy: {
         '/api': {
-          target: 'https://luckless-flogging-stimulate.ngrok-free.dev',
+          target: 'http://localhost:8000',
           changeOrigin: true,
         },
       },
